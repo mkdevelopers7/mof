65f8b4ea9caa484f289be3dc7f10e1bec2281fc0
mkdevelopers7 <mkdevelopers7@gmail.com>
2025-03-04 19:12:09 +0500

    Data Uploaded v5

--- src/features/deposits/AddDeposit.jsx
+++ src/features/deposits/AddDeposit.jsx
@@ -8,7 +8,7 @@ function AddDeposit() {
       <Navigation />
       <div className="mainContainer flex gap-4 flex-col-reverse sm:flex sm:flex-row">
         <SideBar />
-        <div className="mainContainer bg-slate-300 rounded-[15px] mx-auto w-[80%]">
+        <div className="mainContainer bg-slate-300 rounded-[15px] mx-auto w-[95%] sm:w-[80%]">
           <header className="border-b-4 px-3 py-3 border-slate-200">
             <h1 className="text-xl sm:text-2xl text-center text-green-500  font-semibold uppercase">
               Make New Deposit
--- src/features/entries/NewEntry.jsx
+++ src/features/entries/NewEntry.jsx
@@ -19,7 +19,7 @@ function NewEntry() {
       <Navigation />
       <div className="mainContainer flex gap-4 flex-col-reverse sm:flex sm:flex-row">
         <SideBar />
-        <div className="mainContainer bg-slate-300 rounded-[15px] mx-auto w-[80%]">
+        <div className="mainContainer bg-slate-300 rounded-[15px] mx-auto sm:w-[80%] ">
           <header className="border-b-4 px-3 py-3 border-slate-200">
             <h1 className="text-xl sm:text-2xl text-center text-green-500  font-semibold uppercase">
               New Expense
