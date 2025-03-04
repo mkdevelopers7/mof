2223f27271e0bff4ca265b33d52a93c8f82a6d7f
mkdevelopers7 <mkdevelopers7@gmail.com>
2025-03-04 20:17:52 +0500

    Data Uploaded v8

--- src/features/deposits/DepositItem.jsx
+++ src/features/deposits/DepositItem.jsx
@@ -3,32 +3,33 @@ import { formatDate, formatDateShort } from "../../helpers";
 function DepositItem({ deposit, index }) {
   const { id, created_at: date, name, amount } = deposit;
   const dateFinal = formatDateShort(date);
-  return (
-    <div
-      className={`grid gap-2 ${
-        index % 2 === 0 ? "bg-[#d5dce4]" : ""
-      }  grid-cols-[0.5fr,1fr,1fr,1fr] items-center py-2 px-3`}
-    >
-      {/* #cbd5e1 */}
-      <div>
-        <span className=" text-stone-600 text-[14px]">{id}</span>
-      </div>
-      <div>
-        <span className="text-stone-600 text-[14px] capitalize">{name}</span>
-      </div>
-      {/* <div>
+  if (index <= 10)
+    return (
+      <div
+        className={`grid gap-2 ${
+          index % 2 === 0 ? "bg-[#d5dce4]" : ""
+        }  grid-cols-[0.5fr,1fr,1fr,1fr] items-center py-2 px-3`}
+      >
+        {/* #cbd5e1 */}
+        <div>
+          <span className=" text-stone-600 text-[14px]">{id}</span>
+        </div>
+        <div>
+          <span className="text-stone-600 text-[14px] capitalize">{name}</span>
+        </div>
+        {/* <div>
         <span className="text-stone-600 text-[14px] capitalize">{purpose}</span>
       </div> */}
-      <div>
-        <span className="text-stone-600 text-[14px]">{amount}</span>
+        <div>
+          <span className="text-stone-600 text-[14px]">{amount}</span>
+        </div>
+        <div>
+          <span className="text-stone-600 font-semibold sm:font-normal sm:text-[14px] text-[10px]">
+            {dateFinal}
+          </span>
+        </div>
       </div>
-      <div>
-        <span className="text-stone-600 font-semibold sm:font-normal sm:text-[14px] text-[10px]">
-          {dateFinal}
-        </span>
-      </div>
-    </div>
-  );
+    );
 }
 
 export default DepositItem;
--- src/features/entries/EntryItem.jsx
+++ src/features/entries/EntryItem.jsx
@@ -17,46 +17,51 @@ function EntryItem({ entry, index }) {
 
   /////////
 
-  return (
-    <div
-      className={`${
-        index % 2 === 0 ? "bg-[#d5dce4]" : ""
-      }  grid gap-2 grid-cols-[0.5fr,1fr,1fr,1fr,1fr] items-center py-2 space-y-1 px-3 `}
-    >
-      <div>
-        <span className=" text-stone-600 text-[14px]">{id}</span>
-      </div>
-      <div>
-        <span className="text-stone-600 text-[14px] capitalize">
-          {paidBy === "15" ? "Mehran" : ""}
-        </span>
-      </div>
-      <div>
-        <span className="text-stone-600 text-[14px] capitalize">{purpose}</span>
-      </div>
-      <div>
-        <span className="text-stone-600 text-[14px]">{totalAmount}</span>
-      </div>
-      <div>
-        <span className="text-stone-600 text-[14px]">{perHead.toFixed(0)}</span>
-      </div>
-      <div className="col-span-full flex items-center justify-end gap-2 text-[10px] font-normal space-x-2 ">
-        <div className="flex gap-1 items-center">
-          {consumers.map((consumer, i) => (
-            <span
-              className="bg-slate-200 text-stone-500 pb-[1px] px-2 rounded-full"
-              key={i}
-            >
-              {consumer}
-            </span>
-          ))}
+  if (index <= 10)
+    return (
+      <div
+        className={`${
+          index % 2 === 0 ? "bg-[#d5dce4]" : ""
+        }  grid gap-2 grid-cols-[0.5fr,1fr,1fr,1fr,1fr]  py-3  px-3 `}
+      >
+        <div className="">
+          <span className=" text-stone-600 text-[14px]">{id}</span>
         </div>
-        <div className="text-[10px] hidden sm:block font-normal text-stone-600">
-          {formatDateShort(created_at)}
+        <div>
+          <span className="text-stone-600 text-[14px] capitalize">
+            {paidBy === "15" ? "Mehran" : ""}
+          </span>
+        </div>
+        <div>
+          <span className="text-stone-600 text-[14px] capitalize">
+            {purpose}
+          </span>
+        </div>
+        <div>
+          <span className="text-stone-600 text-[14px]">{totalAmount}</span>
+        </div>
+        <div>
+          <span className="text-stone-600 text-[14px]">
+            {perHead.toFixed(0)}
+          </span>
+        </div>
+        <div className="col-span-full flex items-center justify-end gap-2 text-[10px] font-normal space-x-2 ">
+          <div className="flex gap-1 items-center">
+            {consumers.map((consumer, i) => (
+              <span
+                className="bg-slate-200 text-stone-500 pb-[1px] px-2 rounded-full"
+                key={i}
+              >
+                {consumer}
+              </span>
+            ))}
+          </div>
+          <div className="text-[10px] hidden sm:block font-normal text-stone-600">
+            {formatDateShort(created_at)}
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
 }
 
 export default EntryItem;
